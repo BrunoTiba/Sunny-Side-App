8bb5cd9d9edb8da0adea48d73def4e1d8b468889
Bruno Tiba <br.kenji@gmail.com>
2019-12-12 11:02:37 -0300

    Setup ktlint

--- app/src/main/java/com/brunotiba/sunnysideapp/MainActivity.kt
+++ app/src/main/java/com/brunotiba/sunnysideapp/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.brunotiba.sunnysideapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
