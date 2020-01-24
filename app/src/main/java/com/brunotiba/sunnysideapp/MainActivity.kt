17c5507f334549bc5ca561393fa342bcaa316fd3
Bruno Tiba <br.kenji@gmail.com>
2020-01-24 11:30:57 -0300

    Remove test code

--- app/src/main/java/com/brunotiba/sunnysideapp/MainActivity.kt
+++ app/src/main/java/com/brunotiba/sunnysideapp/MainActivity.kt
@@ -2,19 +2,11 @@ package com.brunotiba.sunnysideapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.brunotiba.domain.usecase.GetCurrentWeatherByCityName
-import toothpick.ktp.KTP
-import toothpick.ktp.delegate.inject
 
 class MainActivity : AppCompatActivity() {
-
-    val usecase: GetCurrentWeatherByCityName by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        KTP.openRootScope().inject(this)
-        usecase("hey")
     }
 }
