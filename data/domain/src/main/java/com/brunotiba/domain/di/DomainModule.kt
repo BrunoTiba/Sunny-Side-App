35f563bbef9803b68034a972f28e333599881fe8
Igor Escodro <escodro@outlook.com>
2020-01-27 18:45:53 -0300

    Create Domain representation for Ultraviolet info
    Use case, repository and entity created

--- data/domain/src/main/java/com/brunotiba/domain/di/DomainModule.kt
+++ data/domain/src/main/java/com/brunotiba/domain/di/DomainModule.kt
@@ -1,5 +1,6 @@
 package com.brunotiba.domain.di
 
+import com.brunotiba.domain.usecase.GetCurrentUvByCoordinates
 import com.brunotiba.domain.usecase.GetCurrentWeatherByCityName
 import toothpick.config.Module
 import toothpick.ktp.binding.bind
@@ -8,5 +9,6 @@ val domainModule = object : Module() {
 
     init {
         bind(GetCurrentWeatherByCityName::class).toClass<GetCurrentWeatherByCityName>()
+        bind(GetCurrentUvByCoordinates::class).toClass<GetCurrentUvByCoordinates>()
     }
 }
