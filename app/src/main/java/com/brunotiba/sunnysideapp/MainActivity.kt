63db4b0bdfaf8f8d56fcfd2177d01e59add3ed69
Bruno Tiba <br.kenji@gmail.com>
2020-02-28 15:29:40 -0300

    Undo Activity changes

--- app/src/main/java/com/brunotiba/sunnysideapp/MainActivity.kt
+++ app/src/main/java/com/brunotiba/sunnysideapp/MainActivity.kt
@@ -2,23 +2,11 @@ package com.brunotiba.sunnysideapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.brunotiba.domain.usecase.GetCurrentWeatherByCityName
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import toothpick.ktp.KTP
-import toothpick.ktp.delegate.inject
 
 class MainActivity : AppCompatActivity() {
-
-    val usecase : GetCurrentWeatherByCityName by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        KTP.openRootScope().inject(this)
-        GlobalScope.launch {
-            usecase("Campinas")
-        }
     }
 }
