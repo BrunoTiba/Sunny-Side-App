7b0aba7bb74a72e7513692debc5959ff815ff36a
Bruno Tiba <br.kenji@gmail.com>
2021-09-09 21:47:16 -0300

    Fix formatting
    - Add empty line at end of several files

--- data/local/src/main/java/com/brunotiba/local/dao/LocationDao.kt
+++ data/local/src/main/java/com/brunotiba/local/dao/LocationDao.kt
@@ -1,6 +1,10 @@
 package com.brunotiba.local.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
 import com.brunotiba.local.model.Location
 
 /**
