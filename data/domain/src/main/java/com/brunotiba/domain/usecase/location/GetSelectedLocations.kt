7cbce7ddd5f833afeb6632faec5ea92aafd6950b
Bruno Tiba <br.kenji@gmail.com>
2021-09-13 21:06:33 -0300

    Update Kdoc
    - Update Kdoc on GetSelectedLocations use case

--- data/domain/src/main/java/com/brunotiba/domain/usecase/location/GetSelectedLocations.kt
+++ data/domain/src/main/java/com/brunotiba/domain/usecase/location/GetSelectedLocations.kt
@@ -6,15 +6,19 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 /**
- * Retrieves the list of the locations selected by the user.
+ * Use case to retrieve the list of the locations selected by the user.
  *
  * @param selectedLocationRepository the SelectedLocationRepository
- * @return a Flow that emits the list of the selected locations
  */
 class GetSelectedLocations @Inject constructor(
     private val selectedLocationRepository: SelectedLocationRepository
 ) {
 
+    /**
+     * Retrieves the list of the locations selected by the user.
+     *
+     * @return a Flow that emits the list of the selected locations
+     */
     suspend operator fun invoke(): Flow<List<Location>> {
         return selectedLocationRepository.getSelectedLocations()
     }
