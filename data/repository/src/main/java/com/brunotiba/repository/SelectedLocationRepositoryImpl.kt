d58b711ab910582a0da9ee55727eb63a642fec7b
Bruno Tiba <br.kenji@gmail.com>
2021-09-18 21:49:28 -0300

    Add logging to repository module
    - Add logging to classes in repository module with no logging

--- data/repository/src/main/java/com/brunotiba/repository/SelectedLocationRepositoryImpl.kt
+++ data/repository/src/main/java/com/brunotiba/repository/SelectedLocationRepositoryImpl.kt
@@ -7,6 +7,7 @@ import com.brunotiba.repository.datasource.SelectedLocationDataSource
 import com.brunotiba.repository.mapper.LocationMapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import timber.log.Timber
 import javax.inject.Inject
 
 /**
@@ -19,11 +20,13 @@ class SelectedLocationRepositoryImpl @Inject constructor(
 ) : SelectedLocationRepository {
 
     override suspend fun addSelectedLocation(locationName: String): Long {
+        Timber.d("addSelectedLocation: locationName = $locationName")
         val location = locationDataSource.getLocationFromName(locationName)
         return selectedLocationDataSource.addSelectedLocation(location)
     }
 
     override suspend fun getSelectedLocations(): Flow<List<Location>> {
+        Timber.d("getSelectedLocations")
         return selectedLocationDataSource.getSelectedLocations().map { repoLocations ->
             repoLocations.map { mapper.toDomain(it) }
         }
